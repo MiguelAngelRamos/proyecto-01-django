65a3110342355c3ad4787c3de15dc653c1bc1991
Miguel Ramos <return.infox@gmail.com>
2026-03-13 20:31:02 -0300

    view

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from datetime import datetime
+def lista_productos(request):
 
-# Create your views here.
+    ## Simula la informacion que obtenemos de una base de datos
+    productos = [
+        {'nombre': 'Arroz Grado 1', 'precio': 1200.50, 'stock': 15, 'categoria': 'Abarrotes'},
+        {'nombre': 'Aceite de Oliva', 'precio': 5500.00, 'stock':0, 'categoria': 'Abarrotes'},
+        {'nombre': 'Detergente Líquido', 'precio': 3000.75, 'stock': 8, 'categoria': 'Limpieza'},
+    ]
+
+    contexto = {
+        'fecha_hoy': datetime.now(),
+        'productos': productos,
+        'tienda': 'Minimarket "Sofia"'
+    }    
+    return render(request, 'catalogo/lista.html', contexto)
