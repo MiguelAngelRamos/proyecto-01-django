d1cb79845080f35c87c1602c9d9b23bcfd4e6c69
Miguel Ramos <return.infox@gmail.com>
2026-03-16 20:33:32 -0300

    codigo actualizado

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalogo.urls')),
+    path('bodega/', include('bodega.urls')),
 ]
 
 ## 127.0.0.1:8000/
