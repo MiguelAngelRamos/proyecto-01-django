f158de2d9f23c72a7bf757941be120723f48832d
Miguel Ramos <return.infox@gmail.com>
2026-03-16 19:45:10 -0300

    configuracion de views

--- bodega/views.py
+++ bodega/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+USUARIOS_DB = {
+    'admin': '1234',
+    'sofia': 'academy',
+}
+
+MOVIMIENTOS_DB = [
+    {'tipo': 'Entrada', 'producto': 'Arroz Grado 1',      'cantidad': 50, 'fecha': '10/03/2026', 'responsable': 'admin'},
+    {'tipo': 'Salida',  'producto': 'Aceite de Oliva',    'cantidad':  3, 'fecha': '11/03/2026', 'responsable': 'sofia'},
+    {'tipo': 'Entrada', 'producto': 'Detergente Líquido', 'cantidad': 20, 'fecha': '12/03/2026', 'responsable': 'admin'},
+]
+
+def login_view(request):
+    pass
+
+def logout_view(request):
+    pass
+
+def panel_view(request):
+    pass
