a4199cfd3eac6ee64df7d097b1753397763825a4
Miguel Ramos <return.infox@gmail.com>
2026-03-18 20:11:28 -0300

    movimientos

--- bodega/views.py
+++ bodega/views.py
@@ -1,17 +1,9 @@
 from django.shortcuts import render, redirect
 from datetime import datetime
-from .models import Usuario, Movimiento
-# Create your views here.
-USUARIOS_DB = {
-    'admin': '1234',
-    'sofia': 'academy',
-}
 
-MOVIMIENTOS_DB = [
-    {'tipo': 'Entrada', 'producto': 'Arroz Grado 1',      'cantidad': 50, 'fecha': '10/03/2026', 'responsable': 'admin'},
-    {'tipo': 'Salida',  'producto': 'Aceite de Oliva',    'cantidad':  3, 'fecha': '11/03/2026', 'responsable': 'sofia'},
-    {'tipo': 'Entrada', 'producto': 'Detergente Líquido', 'cantidad': 20, 'fecha': '12/03/2026', 'responsable': 'admin'},
-]
+from catalogo.models import Producto
+from .models import Usuario, Movimiento
+
 
 def login_view(request):
     if 'usuario' in request.session:
@@ -38,25 +30,32 @@ def logout_view(request):
 def panel_bodega(request):
     # Debe verificar si el usuario esta autenticado, sino redirigir al login
     usuario_session = request.session.get('usuario')
-
-    if not usuario_session or usuario_session not in USUARIOS_DB:
+    print(usuario_session)
+    #* SELECT * FROM bodega_usuario WHERE nombre = usuario_session LIMIT 1
+    usuario_objeto = Usuario.objects.filter(nombre=usuario_session).first()
+    if not usuario_session or not usuario_objeto:
         request.session.flush()  # Limpiar la sesión por seguridad
         return redirect('login_bodega')
     
-    if request.method == 'POST':
-        nuevo_movimiento = {
-            'tipo': request.POST.get('tipo', ''),
-            'producto': request.POST.get('producto', '').strip(),
-            'cantidad': int(request.POST.get('cantidad', 0)),
-            'fecha': datetime.now().strftime('%d/%m/%Y'),
-            'responsable': usuario_session,
-        }
-        MOVIMIENTOS_DB.append(nuevo_movimiento)
-        return redirect('panel_bodega')
+    error = None
+    # if request.method == 'POST':
+    #     nuevo_movimiento = {
+    #         'tipo': request.POST.get('tipo', ''),
+    #         'producto': request.POST.get('producto', '').strip(),
+    #         'cantidad': int(request.POST.get('cantidad', 0)),
+    #         'fecha': datetime.now().strftime('%d/%m/%Y'),
+    #         'responsable': usuario_session,
+    #     }
+    #     MOVIMIENTOS_DB.append(nuevo_movimiento)
+    #     return redirect('panel_bodega')
 
+    #  SELECT * FROM bodega_movimiento JOIN catalogo_producto ON bodega_movimiento.producto_id = catalogo_producto.id JOIN bodega_usuario ON bodega_movimiento.responsable_id = bodega_usuario.id esto equivale a -> 'movimientos': Movimiento.objects.select_related('producto', 'responsable').all()
+    
     contexto = {
-            'movimientos': MOVIMIENTOS_DB,
+            'movimientos': Movimiento.objects.select_related('producto', 'responsable').all(),
             'usuario': usuario_session,
-            'total_movimientos': len(MOVIMIENTOS_DB),
+            'total_movimientos': Movimiento.objects.count(),
+            'productos': Producto.objects.all(), # SELECT * FROM catalogo_producto
+            'error': error
     }
     return render(request, 'bodega/panel.html', contexto)
